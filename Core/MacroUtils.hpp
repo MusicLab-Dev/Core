b51b834108320466eb6607cb6c57e4a48da0f507
Matthieu Moinvaziri <m.moinvaziri@gmail.com>
2021-03-25 17:39:45 +0100

    Updated macro utils

--- Core/MacroUtils.hpp
+++ Core/MacroUtils.hpp
@@ -45,7 +45,7 @@
 
 // Iterate of up to 16 arguments, applying a functor
 #define FOR_EACH(what, ...)         _FOR_EACH(VA_ARGC(__VA_ARGS__), what, __VA_ARGS__)
-#define _FOR_EACH(N, what, ...)     _CONCATENATE(_FOR_EACH, N)(what __VA_OPT__(,) __VA_ARGS__)
+#define _FOR_EACH(N, what, ...)     _CONCATENATE(_FOR_EACH_, N)(what __VA_OPT__(,) __VA_ARGS__)
 #define _FOR_EACH_0(what)
 #define _FOR_EACH_1(what, x)        what(x);
 #define _FOR_EACH_2(what, x, ...)   what(x); _FOR_EACH_1(what,  __VA_ARGS__)
@@ -63,6 +63,27 @@
 #define _FOR_EACH_14(what, x, ...)  what(x); _FOR_EACH_13(what, __VA_ARGS__)
 #define _FOR_EACH_15(what, x, ...)  what(x); _FOR_EACH_14(what, __VA_ARGS__)
 #define _FOR_EACH_16(what, x, ...)  what(x); _FOR_EACH_15(what, __VA_ARGS__)
+
+// Iterate of up to 16 arguments, applying a functor
+#define FOR_COMMA_EACH(what, ...)         _FOR_COMMA_EACH(VA_ARGC(__VA_ARGS__), what, __VA_ARGS__)
+#define _FOR_COMMA_EACH(N, what, ...)     _CONCATENATE(_FOR_COMMA_EACH_, N)(what __VA_OPT__(,) __VA_ARGS__)
+#define _FOR_COMMA_EACH_0(what)
+#define _FOR_COMMA_EACH_1(what, x)        what(x)
+#define _FOR_COMMA_EACH_2(what, x, ...)   what(x), _FOR_COMMA_EACH_1(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_3(what, x, ...)   what(x), _FOR_COMMA_EACH_2(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_4(what, x, ...)   what(x), _FOR_COMMA_EACH_3(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_5(what, x, ...)   what(x), _FOR_COMMA_EACH_4(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_6(what, x, ...)   what(x), _FOR_COMMA_EACH_5(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_7(what, x, ...)   what(x), _FOR_COMMA_EACH_6(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_8(what, x, ...)   what(x), _FOR_COMMA_EACH_7(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_9(what, x, ...)   what(x), _FOR_COMMA_EACH_8(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_10(what, x, ...)  what(x), _FOR_COMMA_EACH_9(what,  __VA_ARGS__)
+#define _FOR_COMMA_EACH_11(what, x, ...)  what(x), _FOR_COMMA_EACH_10(what, __VA_ARGS__)
+#define _FOR_COMMA_EACH_12(what, x, ...)  what(x), _FOR_COMMA_EACH_11(what, __VA_ARGS__)
+#define _FOR_COMMA_EACH_13(what, x, ...)  what(x), _FOR_COMMA_EACH_12(what, __VA_ARGS__)
+#define _FOR_COMMA_EACH_14(what, x, ...)  what(x), _FOR_COMMA_EACH_13(what, __VA_ARGS__)
+#define _FOR_COMMA_EACH_15(what, x, ...)  what(x), _FOR_COMMA_EACH_14(what, __VA_ARGS__)
+#define _FOR_COMMA_EACH_16(what, x, ...)  what(x), _FOR_COMMA_EACH_15(what, __VA_ARGS__)
 
 // Automatically give names to a list of up to 16 arguments
 #define NAME_EACH(...)              _NAME_EACH(VA_ARGC(__VA_ARGS__), __VA_ARGS__)
